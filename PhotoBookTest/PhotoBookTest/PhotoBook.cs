0d14b0e0e57b0bd5b4bdce359ad4b3f1fcd680c7
ChheanBrosna <phalnhok@gmail.com>
2022-03-07 14:33:09 +0700

    update activities OOP

---
+++ PhotoBookTest/PhotoBookTest/PhotoBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBookTest
+{
+    class PhotoBook
+    {
+        protected int numberPages; // init 
+
+        public PhotoBook()
+        {
+            numberPages = 16;
+        }
+        public PhotoBook(int numberPages) // using parameter, means it will user will input data later
+        {
+            this.numberPages = numberPages; // user this coz we user parameter
+        }
+        public int getNumber()
+        {
+            return numberPages; // return whateve value from numberPage input
+        }
+    }
+}
---
+++ PhotoBookTest/PhotoBookTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PhotoBookTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhotoBook myAlbum = new PhotoBook(); 
+            int x = myAlbum.getNumber();
+            Console.WriteLine($"First photo book is {x}"); // output 16
+
+
+            PhotoBook myAlbum2 = new PhotoBook(23); /// if not input value, automatically run 16 
+            int y = myAlbum2.getNumber();
+            Console.WriteLine($"Second photo book is {y}");
+
+
+            SuperPhotoBook myBigAlbum = new SuperPhotoBook();
+            int s = myBigAlbum.getNumber();
+            Console.WriteLine($"Super Photo Book is {s}"); // out 45
+
+
+        }
+    }
+}
